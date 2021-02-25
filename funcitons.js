bed205a2fa750c5c2a227e0791d2cfb09b187e67
Harman Singh <singhharman160201@gmail.com>
2021-02-26 00:21:36 +0530

    about ready

--- funcitons.js
+++ funcitons.js
@@ -14,6 +14,28 @@ if(screen.width > 738){
         else return closeNav()
 }
 }
+window.onscroll = function() {displayAbout()};
+
+function displayAbout(){
+        if(screen.width > 340 && screen.width < 740){
+        if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+                        document.getElementById("about").style.opacity = "1"
+                        document.getElementById("about").style.transform = "translate(0px,10px)"
+            }
+        }
+        if(screen.width > 738){
+                if(document.body.scrollTop > 90 || document.documentElement.scrollTop > 90){
+                                document.getElementById("about").style.opacity = "1"
+                                document.getElementById("about").style.transform = "translate(0px,-30px)"
+                        }
+                }
+                if(screen.width > 1000){
+                        if(document.body.scrollTop > 120 || document.documentElement.scrollTop > 120){
+                                        document.getElementById("about").style.opacity = "1"
+                                        document.getElementById("about").style.transform = "translate(0px,-60px)"
+                                }
+                        }
+} 
 // document.addEventListener('click', closeNav)
 function closeNav() {
         document.getElementById("sideNav").style.width = "0px"
